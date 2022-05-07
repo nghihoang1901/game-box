c21cebc44392b1ad080d744ee943ac7de906f24e
Huu Nghi <hoanghuunghi113@gmail.com>
2022-05-07 17:05:59 +0700

    game-box

---
+++ review/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Content from './widgets/Content';
+import Footer from './widgets/Footer';
+import Header from './widgets/Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ review/src/widgets/Content.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return (
+      <div>
+        <div class="review">
+          <div class="container">
+            <h2>Reviews</h2>
+            <div class="review-md1">
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re7.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re1.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re2.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="clearfix"> </div>
+            </div>
+            <div class="review-md1">
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re3.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re4.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re5.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="clearfix"> </div>
+            </div>
+            <div class="review-md1">
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re6.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re7.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4 sed-md">
+                <div class=" col-1">
+                  <a href="single.html">
+                    <img class="img-responsive" src="images/re2.jpg" alt="" />
+                  </a>
+                  <h4>
+                    <a href="single.html">Sed ut perspiciatis unde omnis</a>
+                  </h4>
+                  <p>
+                    Lorem Ipsum is not simply random text. It has roots in a
+                    piece of classical Latin literature
+                  </p>
+                </div>
+              </div>
+              <div class="clearfix"> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Content;
