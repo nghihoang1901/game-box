ae4428668c883fcdcbc0ae37e87be86d59c95d2c
Huu Nghi <hoanghuunghi113@gmail.com>
2022-05-08 07:46:49 +0700

    game-box

---
+++ gallery/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Gallery from './widgets/Gallery';
+import Header from "./widgets/Header";
+import Footer from "./widgets/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Gallery></Gallery>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ gallery/src/widgets/Gallery.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return (
+      <div>
+        <div className="gallery">
+          <div className="container">
+            <h2>Gallery</h2>
+            <div className="gallery-bottom">
+              <div className="gallery-1">
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/r4.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/r4.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl7.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl7.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl4.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl4.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl5.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl5.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="clearfix"></div>
+              </div>
+              <div className="gallery-1">
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl6.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl6.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl2.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl2.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/r6.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/r6.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl7.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl7.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="clearfix"></div>
+              </div>
+              <div className="gallery-1">
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl4.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl4.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/r3.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/r3.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/r4.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/r4.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="col-md-3 gallery-grid">
+                  <a
+                    className="example-image-link"
+                    href="images/gl11.jpg"
+                    data-lightbox="example-set"
+                    data-title="Click the right half of the image to move forward."
+                  >
+                    <img className="example-image" src="images/gl11.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="clearfix"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
