958a45a3cb28ac0291efa54b1dc288a5bd66ad93
Huu Nghi <hoanghuunghi113@gmail.com>
2022-05-18 07:16:39 +0700

    game-box

--- game-box/src/Page/DSProducts.js
+++ game-box/src/Page/DSProducts.js
@@ -6,6 +6,7 @@ class DsProducts extends Component {
     super(props);
     this.state = {
       ds_sp: [],
+      so_trang: 0
     };
   }
 
@@ -22,13 +23,53 @@ class DsProducts extends Component {
       .catch((error) => {
         console.log(error);
       })
+      .then(function () 
+      {});
+
+      axios.get("http://localhost:4000/products/total-page")
+      .then((response) => {
+        // console.log(response);
+        this.setState((preState) => {
+          preState.so_trang = response.data.so_trang;
+          return preState;
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(function () {});
+  }
+
+  handleChangePage = (e) => {
+    // console.log(e.target.getAttribute('data-page'));
+    let page_chose = e.target.getAttribute('data-page')
+    axios
+      .get("http://localhost:4000/products?page=" + page_chose)
+      .then((response) => {
+        // console.log(response);
+        this.setState((preState) => {
+          preState.ds_sp = response.data;
+          return preState;
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
       .then(function () {});
   }
 
   render() {
+
+    let ds_trang= [];
+    for(var i = 0 ; i< this.state.so_trang; i++){
+      ds_trang.push(i);
+    }
+
     return (
       <div>
-        <div>Trang danh sách sản phẩm</div>
+        <div>Trang danh sách sản phẩm
+        {this.state.so_trang}
+        </div>        
         <div>
           {this.state.ds_sp.map((itemProducts) => {
             return (
@@ -38,7 +79,19 @@ class DsProducts extends Component {
             );
           })}
         </div>
-        
+        <div className="phan_trang">          
+          <button type="button" class="btn btn-primary">first</button>
+          <button type="button" class="btn btn-primary">prev</button>
+          <div className="ds_trang">
+            {
+              ds_trang.map(item_trang => {
+                return <button type="button" onClick={this.handleChangePage} data-page = {item_trang} class="btn btn-primary">{item_trang + 1}</button>
+              })
+            }
+          </div>
+          <button type="button" class="btn btn-primary">next</button>
+          <button type="button" class="btn btn-primary">last</button>  
+        </div>
       </div>
     );
   }
